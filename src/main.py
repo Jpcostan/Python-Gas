057cc17c0d73a33727e81985615a9a8b27a6777f
Joshua <costanzajosh@gmail.com>
2021-10-24 01:22:41 -0400

    Shell algorithm completed
    In it's current form, this script contains a while loop that does not accommodate for any user input or dependencies to be ran externally (via a daemon, job or executable). It will need to be configured to have actual utility. Please see comments within the main.py class.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
 import requests
-import pprint
-import json 
+import pprint 
 
-r = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=##YOUR-KEY_HERE##')
-#print(r.json())
-#pprint.pprint(r.json())
-#need to parse the json to only print 'average' key and val
+#Request with EthGasStation API key.
+#You will need to create an account and input key in the string below.
+r = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=f8c65ec148a5a195b73f00241810944500a7fe7fa04b073b0c8c118b00ca')
 
-#capture the json from request
+#Capture the json from request.
 x = r.json()
-#print average gas from the json obj
+#Print average gas from the json obs.
 print("Current gas average :" + str(x['average']))
+
+###TODO
+###Define criteria for which the script will run in the background
+###Every hour? Every 30 minuets? Every 15 minuets?
+###This will help determine if this should run as daemon, a job or
+###be ran via executable.
+###Configure while loop below to accomodate for how this script will be ran.
+
+
+#This is an infinite loop. It will continuously execute in it's current form
+#unless configured otherwise. 
+while(1):
+    r = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=f8c65ec148a5a195b73f00241810944500a7fe7fa04b073b0c8c118b00ca')
+    x = r.json()
+    gas = x['average']
+    if gas <= 650:
+        print("Alert! Curent gas is LOW: " + str(x['average']))
+
+###TODO
+###Send alert to window pop-up
